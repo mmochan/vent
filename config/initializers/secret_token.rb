fa87922ae5b996e21dc29bc596db013358a8516a
Mike Mochan <mmochan@mac.com>
2013-08-27 20:38:18 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vent::Application.config.secret_key_base = '78e40af7d3f349744025b1956fe4f6f0aaa91ca7000196ca80b7630e5d964576ddf33eea6b5ad9bbb390ebef39b6162d9dba5641e552c1c06d98c1bdc366248e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vent::Application.config.session_store :cookie_store, key: '_vent_session'
